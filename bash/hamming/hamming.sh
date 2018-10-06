762b0dd1d85b917f19af05327ce96ae29979ebae
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-06 11:53:33 -0700

    Bash: Hamming

---
+++ bash/hamming/hamming.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 2 ]]; then
+  echo "Usage: hamming.sh <strand1> <strand2>"
+  exit 1
+fi
+
+s1="$1"
+s2="$2"
+
+if (( ${#s1} != ${#s2} )); then
+ echo "left and right strands must be of equal length" 
+ exit 1
+fi
+
+count=0
+for ((i=0;i<${#s1};i++)); do
+  if [[ ${s1:$i:1} != ${s2:$i:1} ]]; then
+    ((count+=1))
+  fi
+done
+echo $count
+
