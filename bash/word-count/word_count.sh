f4216dd5a5e3384aa5901484b40a92d42f00edaa
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-07 12:19:56 -0700

    Bash: Word Count (lowercase part of clean_word())

--- bash/word-count/word_count.sh
+++ bash/word-count/word_count.sh
@@ -8,7 +8,7 @@ words=${words//'\n'/ }
 # strip the given word of extra symbols
 # TODO: combine these regexes into one.
 clean_word () {
-  w="$1"
+  w="${1,,}"
   [[ $w =~ ^[[:alnum:]\']+        ]] && w=${BASH_REMATCH[0]}
   [[ $w =~ ^\'([[:alnum:]\']+)\'$ ]] && w=${BASH_REMATCH[1]}
   echo "$w"
@@ -16,14 +16,14 @@ clean_word () {
 
 seen=","
 for w in $words; do
-  declare -l w=$(clean_word $w)
+  w=$(clean_word $w)
 
   [[ $seen =~ ,$w, ]] && continue
   seen+="$w,"
 
   count=0
   for w2 in $words; do
-    declare -l w2=$(clean_word $w2)
+    w2=$(clean_word $w2)
     [[ $w == $w2 ]] && ((count++))
   done
   echo "$w: $count"
