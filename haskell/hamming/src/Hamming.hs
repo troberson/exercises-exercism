21d85f8d6bc2f56c0badec5a4a2bbce88d1a7adb
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-13 10:52:52 -0700

    Haskell: Hamming

---
+++ haskell/hamming/src/Hamming.hs
@@ -0,0 +1,8 @@
+module Hamming (distance) where
+
+distance :: String -> String -> Maybe Int
+distance xs ys
+  | length xs /= length ys = Nothing
+  | otherwise =
+      Just . length . filter id $ zipWith (/=) xs ys
+
