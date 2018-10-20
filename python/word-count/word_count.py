cb93c1401911c18f8ea09701625eabc7387a51be
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-19 19:17:19 -0700

    Python: Word Count (Return using ternary)

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -6,13 +6,9 @@ def word_count(phrase_orig: str) -> dict:
     phrase: str = re.sub(r'[,\n,_]', ' ', phrase_orig)
     phrase_list: List[str] = list(map(clean_word, phrase.split()))
     words: Set[str] = set(phrase_list)
-
     return dict([(word, phrase_list.count(word)) for word in words if word])
 
 
 def clean_word(word: str) -> str:
     match: Optional[Match] = re.match(r"\'?([0-9a-z\']+)\b\'?", word.lower())
-    if not match:
-        return ''
-
-    return match.group(1)
+    return match.group(1) if match else ''
