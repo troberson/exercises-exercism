7c965c9744b286cbd5ed9fab23e393d2da11821d
Tamara Roberson <tamara.roberson@gmail.com>
2018-10-19 19:20:39 -0700

    Python: Word Count (Don't bother replacing \n)

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -3,7 +3,7 @@ from typing import List, Match, Optional, Set
 
 
 def word_count(phrase_orig: str) -> dict:
-    phrase: str = re.sub(r'[,\n,_]', ' ', phrase_orig)
+    phrase: str = re.sub(r'[,_]', ' ', phrase_orig)
     phrase_list: List[str] = list(map(clean_word, phrase.split()))
     words: Set[str] = set(phrase_list)
     return dict([(word, phrase_list.count(word)) for word in words if word])
